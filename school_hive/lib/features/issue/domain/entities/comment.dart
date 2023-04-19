880d3b17bee7ad72889d383f2360fc52b028daf3
Eyob14 <eyobzebene3580@gmail.com>
2023-04-19 16:32:52 +0300

    comment entity

---
+++ school_hive/lib/features/issue/domain/entities/comment.dart
@@ -0,0 +1,25 @@
+import 'package:equatable/equatable.dart';
+
+class Comment extends Equatable {
+  final String id;
+  final String description;
+  final int upVotes;
+  final int downVotes;
+  final DateTime time;
+  
+  Comment({
+    required this.id,
+    required this.description,
+    required this.upVotes,
+    required this.downVotes,
+    required this.time,
+  });
+  @override
+  List<Object?> get props => [
+        id,
+        description,
+        upVotes,
+        downVotes,
+        time,
+      ];
+}
