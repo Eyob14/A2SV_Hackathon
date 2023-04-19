e08f395e5b5643a69c9c1f85a54260f0b8c475c6
JohnZDictator <yohannesketemazeleke@gmail.com>
2023-04-19 12:44:28 +0300

    login logic

--- school_hive/lib/core/utils/image_uploader.dart
+++ school_hive/lib/core/utils/image_uploader.dart
@@ -1,41 +1,41 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/services.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:flutter/services.dart';
+// import 'package:image_cropper/image_cropper.dart';
+// import 'package:image_picker/image_picker.dart';
 
-class ImageUploader {
-  Future<File?> getImage(ImageSource source,
-      [bool defaultAspectRatio = true]) async {
-    try {
-      final image = await ImagePicker().pickImage(source: source);
-      if (image == null) return null;
-      final imageTemp = File(image.path);
-      final croppedImage = await _cropImage(
-        imageFile: imageTemp,
-        defaultAspectRatio: defaultAspectRatio,
-      );
-      return croppedImage;
-    } on PlatformException {
-      print('platform dependent error eccured while taking picture');
-      return null;
-    }
-  }
+// class ImageUploader {
+//   Future<File?> getImage(ImageSource source,
+//       [bool defaultAspectRatio = true]) async {
+//     try {
+//       final image = await ImagePicker().pickImage(source: source);
+//       if (image == null) return null;
+//       final imageTemp = File(image.path);
+//       final croppedImage = await _cropImage(
+//         imageFile: imageTemp,
+//         defaultAspectRatio: defaultAspectRatio,
+//       );
+//       return croppedImage;
+//     } on PlatformException {
+//       print('platform dependent error eccured while taking picture');
+//       return null;
+//     }
+//   }
 
-  // this is for image cropper
-  Future<File?> _cropImage(
-      {required File imageFile, bool? defaultAspectRatio}) async {
-    CroppedFile? croppedImage = await ImageCropper().cropImage(
-      compressFormat: ImageCompressFormat.png,
-      sourcePath: imageFile.path,
-      aspectRatio: defaultAspectRatio!
-          ? const CropAspectRatio(
-              ratioX: 1100,
-              ratioY: 650,
-            )
-          : null,
-    );
-    if (croppedImage == null) return null;
-    return File(croppedImage.path);
-  }
-}
+//   // this is for image cropper
+//   Future<File?> _cropImage(
+//       {required File imageFile, bool? defaultAspectRatio}) async {
+//     CroppedFile? croppedImage = await ImageCropper().cropImage(
+//       compressFormat: ImageCompressFormat.png,
+//       sourcePath: imageFile.path,
+//       aspectRatio: defaultAspectRatio!
+//           ? const CropAspectRatio(
+//               ratioX: 1100,
+//               ratioY: 650,
+//             )
+//           : null,
+//     );
+//     if (croppedImage == null) return null;
+//     return File(croppedImage.path);
+//   }
+// }
